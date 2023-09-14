d8de15c19a4a3e36f9a35bb28542861529d12db4
CodeStation5 <js21.xda@gmail.com>
2023-09-13 22:57:27 -0700

    setup base files

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+namespace MoviesMenu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // When the button is clicked, this will be executed
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
